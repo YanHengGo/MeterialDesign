a3fc86d03e4fe64824c619164632a226d8273c13
YanHengGo <dang712@hotmail.com>
2019-01-06 15:24:41 +0900

    Rxjava 执行定时任务

--- 02_fragmentViewpager/app/src/main/java/com/yanheng/fragmentdemo/MainActivity.java
+++ 02_fragmentViewpager/app/src/main/java/com/yanheng/fragmentdemo/MainActivity.java
@@ -8,14 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import com.yanheng.fragmentdemo.adapter.MypagerAdapter;
+import com.yanheng.fragmentdemo.util.L;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
 
     ArrayList<View> viewlist = new ArrayList<>();
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager = ((ViewPager) findViewById(R.id.viewpager));
         initView();
         viewPager.setAdapter(new MypagerAdapter(viewlist));
+        setTimer();
     }
 
     private void initView() {
@@ -38,5 +47,48 @@ public class MainActivity extends AppCompatActivity {
     private void addViewWithId(int viewpager) {
         View view = View.inflate(this, viewpager, null);
         viewlist.add(view);
+    }
+
+    private void setTimer(){
+        Observable.interval(2000,TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable disposable) {
+                        MainActivity.this.disposable = disposable;
+                        L.d();
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        L.d();
+                        int currentItem = viewPager.getCurrentItem();
+                        currentItem++;
+                        if(currentItem==viewlist.size()){
+                            currentItem=0;
+                        }
+                        viewPager.setCurrentItem(currentItem);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        L.d();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        L.d();
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        L.d();
+        if(disposable!=null && !disposable.isDisposed()){
+            L.d();
+            disposable.dispose();
+        }
     }
 }
